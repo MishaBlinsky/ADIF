adabd6b230d75543c4cc6230b55a80bf9b27fef7
MishaBlinsky <ritikutvarfa@gmail.com>
2025-02-08 02:04:09 +0300

    added more spaces

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
+
 int main() {
-    std::string name; // строка для имени
-    std::cout << "Enter your name: "; // просьба ввести имя
-    std::cin >> name; // получение инпута
+
+    std::string name;  // строка для имени
+
+    std::cout << "Enter your name: ";// просьба ввести имя
+
+    std::cin >> name;// получение инпута
+
     std::cout << "Hello world from " << name << endl; // печать hello world
+  
     return 0; // end
+
 }
-
-
